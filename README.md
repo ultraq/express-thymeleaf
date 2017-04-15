590b51bfb660b1188ee32438c33429a9b308a0a4
Emanuel Rabina <emanuelrabina@gmail.com>
2017-04-15 17:44:16 +1200

    Extract bridge function from thymeleaf-js project

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# express-thymeleaf
-Integrate ThymeleafJS with the Express web framework
+
+Express Thymeleaf
+=================
+
+Integrate [ThymeleafJS](https://github.com/ultraq/thymeleaf-js) with
+[Express](http://expressjs.com/).
+
+ThymeleafJS returns Promises of processed templates, but Express uses a callback
+mechanism for receiving rendered/processed HTML.  This tiny module saves you
+having to do the boilerplate of stitching those 2 together to get them to work.
+
+
+Installation
+------------
+
+```
+npm install express-thymeleaf --save
+```
+
+
+Usage
+-----
+
+> This module is still under development, denoted by the 0.x semver, in
+> conjunction with the thymeleaf-js project, so expect anything below to change.
+
+```javascript
+import express          from 'express';
+import expressThymeleaf from 'express-thymeleaf';
+import thymeleaf        from 'thymeleaf';
+
+// Configure your application to use Thymeleaf via the express-thymeleaf module
+let app = express();
+app.engine('html', expressThymeleaf(thymeleaf));
+app.set('view engine', 'html');
+
+// Render views as you would normally in response to requests
+app.get('/', function(request, response) {
+	response.render('index');
+});
+
+app.listen(3000);
+```
---
+++ express-thymeleaf.js
@@ -0,0 +1,39 @@
+/* 
+ * Copyright 2017, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+/**
+ * Bridges the Promise value of Thymeleaf's `process*` functions and Express'
+ * callback for rendering.
+ * 
+ * @param {Thymeleaf} thymeleaf
+ *   The Thymeleaf module to use for rendering templates.
+ * @return {Function}
+ *   An Express-compatible template engine function that uses Thymeleaf for
+ *   template rendering, executing the callback with the appropriate values for
+ *   the rendering response.
+ */
+module.exports = function(thymeleaf) {
+	return function(filePath, options, callback) {
+		thymeleaf.processFile(filePath, options)
+			.then(template => {
+				callback(null, template);
+			})
+			.catch(error => {
+				callback(error);
+			});
+	};
+};
